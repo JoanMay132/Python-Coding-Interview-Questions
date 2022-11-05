4c5e560f0d882bc385546295ae5aece706437282
Joan May <jcmd270@gmail.com>
2022-11-05 11:26:32 -0600

    subo archivo

--- Floating_Point_Comparison/Floating_Point_Comparison.py
+++ Floating_Point_Comparison/Floating_Point_Comparison.py
@@ -1,6 +1,7 @@
 def approx_equals(a, b):
-    a = round(a, 4)
-    b = round(b, 4)
-    return a == b
+    a = round(a, 5)
+    b = round(b, 5)
+    float_value=0.001
+    return abs(a-b) < float_value
 
 print(approx_equals(175.9827, 82.25))
